6ece3e9010ddf1e4f14b90e84655d6f3d9957e37
Glenn Welser <glenn.welser@gmail.com>
2016-04-26 12:42:56 -0400

    gulp-ify scripts

--- SAT-styles.php
+++ SAT-styles.php
@@ -6,12 +6,12 @@ function SAT_styles() {
 	global $post;
 
 	// Make sure the shortcode exists first.
-	if ( is_post_type_archive() || !has_shortcode( $post->post_content, 'gallery') )
-		return false;
+	if ( is_post_type_archive() || ! has_shortcode( $post->post_content, 'gallery' ) ) {
+		return;
+	}
 
-	wp_enqueue_style( 'sat_styles', SAT_URL.'css/style.css' );
-	wp_enqueue_script( 'froogaloop', SAT_URL.'scripts/vendors/froogaloop.min.js', '', '', true );
-	wp_enqueue_script( 'sat_scripts', SAT_URL.'scripts/min/sat_gallery-ck.js', '', '', true );
+	wp_enqueue_style( 'sat_styles', SAT_URL . 'css/style.css' );
+	wp_enqueue_script( 'sat_scripts', SAT_URL . 'js/sat_gallery.min.js', '', '', true );
 }
 
 add_action( 'wp_enqueue_scripts', 'SAT_styles', 11 );
--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,12 @@
 var gulp = require('gulp');
 
 // Include Our Plugins
+var concat = require('gulp-concat');
 var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
 
 // Compile Our Sass
-gulp.task('sass', function () {
+gulp.task('build-styles', function () {
     return gulp.src('./css/pre/style.scss')
         .pipe(sass({
             outputStyle: 'compressed'
@@ -14,5 +15,12 @@ gulp.task('sass', function () {
         .pipe(gulp.dest('./css'));
 });
 
+gulp.task('build-scripts', function () {
+    return gulp.src(['./js/pre/vendor/**/*.js', './js/pre/*.js'])
+        .pipe(concat('sat_gallery.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./js'));
+});
+
 // Default Task
-gulp.task('default', ['sass']);
+gulp.task('default', ['build-styles', 'build-scripts']);
